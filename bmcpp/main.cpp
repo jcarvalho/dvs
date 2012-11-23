36f721606729521288af11439c32f019a68b727e
Joao Carvalho <joao.pedro.carvalho@ist.utl.pt>
2012-11-23 15:43:50 +0000

    Initial Commit

---
+++ bmcpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  bmcpp
+//
+//  Created by Joao Carvalho on 11/23/12.
+//  Copyright (c) 2012 IST. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
